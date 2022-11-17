f89fb069ce5433e431f4b4a8649dff30f8345545
Justin Lopato <justin@lopato.org>
2022-11-16 23:16:07 -0600

    refinements

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,139 +6,136 @@ using UnityEngine.Experimental.GlobalIllumination;
 
 public class Movement : MonoBehaviour
 {
+    // Constant multiplier that matches mouse sensitivity values to other similar games
+    private const float standardMultiplier = 26.33405852f;
+
+    // Objects from game scene that need to be referenced by this class
     private CharacterController Player;
-    public Transform Eyes;
-    public Transform Feet;
+    public Transform Camera;
     public LayerMask Ground;
 
-    public float speed;
-    public float gravity;
-    public float jump;
-    public float sensitivity;
+    // Multiplier values initialized in Unity script menu
+    public float walkSpeed;
+    public float gravityAcceleration;
+    public float jumpSpeed;
+    public float mouseSensitivity;
 
+    // Velocity applied to player each frame
     private Vector3 velocity;
-    private int orientation = 1;
-    private int orientation2 = 1;
-    private float sprinting = 1f;
+
+    // Rotation value about x axis of camera
     private float verticalRotation = 0f;
+
+    // Private values that change every frame based on rotation & collisions
+    private int playerOrientation = 1;
+    private int gravityDirection = 1;
     bool grounded = false;
 
+    // Runs at start of game
     void Start()
     {
+        // Keeps mouse cursor in center of screen & hides it
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
+        // Assigns CharacterController component of the object this script is assigned to to variable Player
         Player = GetComponent<CharacterController>();
     }
 
+    // Runs every frame
     void Update()
     {
-        GroundCheck();
-        UpCheck();
-        SprintCheck();
+        //Debug.Log("playerOrientation: " + (playerOrientation == 1 ? "Up" : "Down"));
+        //Debug.Log("gravityDirection: " + (gravityDirection == 1 ? "Up" : "Down"));
+        Debug.Log("grounded: " + (grounded ? "Yes" : "No"));
+
+        // 
+        CollisionCheck();
+        OrientationCheck();
+
         Looking();
         Walking();
         Falling();
+        Jumping();
 
+        // Applies velocity to player multiplied by time passed since last frame()
         Player.Move(velocity * Time.deltaTime);
     }
 
-    void GroundCheck()
+    void CollisionCheck()
     {
-        //grounded = Physics.SphereCheck(Feet, .1f, Ground);
+        grounded = Physics.CheckCapsule(transform.position + transform.up * .6f, transform.position - transform.up * .6f, .49f, Ground);
     }
 
-    void UpCheck()
+    void OrientationCheck()
     {
         while (verticalRotation < 0f)
         {
             verticalRotation += 360f;
         }
 
-        Debug.Log(verticalRotation % 360);
+        //Debug.Log(verticalRotation % 360);
 
         float rotation = verticalRotation % 360;
 
         if (rotation > 90f && rotation < 270f)
         {
-            orientation = -1;
+            playerOrientation = -1;
         } 
         else if(rotation < 90f ^ rotation > 270f)
         {
-            orientation = 1;
+            playerOrientation = 1;
         }
 
         if (rotation > 0f && rotation < 180f)
         {
-            orientation2 = 1;
+            gravityDirection = 1;
         }
         else if (rotation > 180f && rotation < 360f)
         {
-            orientation2 = -1;
+            gravityDirection = -1;
         }
-
-        //Debug.Log(orientation);
-    }
-
-    void SprintCheck()
-    {
-        sprinting = Input.GetButton("Shift") ? 1.3f : 1f;
     }
 
     void Looking()
     {
-        float x = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
-        float y = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+        float x = Input.GetAxis("Mouse X") * mouseSensitivity * standardMultiplier * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * mouseSensitivity * standardMultiplier * Time.deltaTime;
 
         verticalRotation -= y;
 
-        Eyes.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
-        transform.Rotate(Vector3.up * x * orientation);
+        Camera.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * x * playerOrientation);
     }
 
     void Walking()
     {
         float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical") * orientation;
+        float v = Input.GetAxis("Vertical") * playerOrientation;
 
         Vector3 move = transform.forward * v + transform.right * h;
 
-        float temp = velocity.y;
-        velocity = move * speed * sprinting;
-        velocity.y = temp;
+        velocity.x = move.x * walkSpeed;
+        velocity.z = move.z * walkSpeed;
     }
 
     void Jumping()
     {
         if (grounded && Input.GetButton("Jump"))
         {
-            velocity.y = jump * orientation;
+            velocity.y = jumpSpeed * playerOrientation;
         }
     }
 
     void Falling()
     {
-        velocity.y += gravity * orientation2 * (grounded ? 0f : 1f) * Time.deltaTime;
-
-        if (grounded && orientation2 == orientation)
+        if (grounded && gravityDirection == playerOrientation)
         {
             velocity.y = 0f;
         }
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.layer.Equals(Ground))
+        else
         {
-            grounded = true;
-        }
-    }
-
-    void OnCollisionExit(Collision col)
-    {
-        if (col.gameObject.layer.Equals(Ground))
-        {
-            grounded = false;
+            velocity.y -= gravityAcceleration * gravityDirection * Time.deltaTime;
         }
     }
 }
